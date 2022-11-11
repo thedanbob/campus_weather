fee2115a22a3ed7156f4efa906657b0140e099b5
Dan Arnfield <dan@arnfield.com>
2022-11-11 09:58:53 -0600

    Inital commit: ruby and python versions

---
+++ weather.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# Usage: env TOKEN=<token> ./weather.py <url>
+
+import json, os, re, os, struct, sys, time # stdlib
+import requests, serial
+
+FORECASTS = {
+    8: 'sunny',
+    6: 'partlycloudy',
+    2: 'cloudy',
+    3: 'rainy',
+    18: 'snowy',
+    19: 'snowy-rainy',
+    7: 'rainy',
+    22: 'snowy',
+    23: 'snowy-rainy',
+}
+
+def crc16_ccitt(data, init_crc=0):
+    msb = init_crc >> 8
+    lsb = init_crc & 255
+    for b in data:
+        x = b ^ msb
+        x ^= (x >> 4)
+        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
+        lsb = (x ^ (x << 5)) & 255
+    return (msb << 8) + lsb
+
+socket = serial.Serial(port="/dev/ttyUSB0", baudrate=19200, timeout=1.2)
+
+for _ in range(3):
+    socket.write(b'\n')
+    if socket.read(2) == b'\n\r':
+        break
+else:
+  print('Station not ready')
+  socket.close()
+  sys.exit(1)
+
+socket.write(b'LOOP 1\n')
+packet = socket.read(100)
+socket.close()
+
+if len(packet) < 100:
+    print('Received incomplete data')
+    sys.exit(1)
+
+data = re.fullmatch(b'\x06(LOO.+\n\r.{2})', packet, flags=re.DOTALL)
+if data == None or crc16_ccitt(data[1]) != 0:
+    print('Received malformed packet')
+    sys.exit(1)
+
+# Extract bytes 8-9, 13-14, 15, 34, 90 (little-endian)
+decoded = struct.unpack('<7xH3xHB18xB55xB9x', data[1])
+
+requests.post(
+    sys.argv[0],
+    headers={
+        'Authorization': 'Bearer {}'.format(os.environ['TOKEN']),
+        'Content-Type': 'application/json'
+    },
+    data=json.dumps({
+        'state': FORECASTS[decoded[4]],
+        'attributes': {
+            'friendly_name': 'Campus Weather',
+            'pressure': decoded[0] / 1000,
+            'temperature': decoded[1] / 10,
+            'wind_speed': decoded[2],
+            'humidity': decoded[3],
+            'last_update': time.strftime('%Y-%m-%dT%H:%M:%S%z'),
+        }
+    })
+)
---
+++ weather.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+# Usage: env TOKEN=<token> ./weather.rb <url>
+
+require 'json' # stdlib
+require 'net/http' #stdlib
+require 'serialport'
+
+Forecasts = {
+  8 => 'sunny',
+  6 => 'partlycloudy',
+  2 => 'cloudy',
+  3 => 'rainy',
+  18 => 'snowy',
+  19 => 'snowy-rainy',
+  7 => 'rainy',
+  22 => 'snowy',
+  23 => 'snowy-rainy'
+}
+
+def crc16_ccitt(data, init_crc=0)
+  msb = init_crc >> 8
+  lsb = init_crc & 255
+
+  data.each_byte do |b|
+    x = b ^ msb
+    x ^= (x >> 4)
+    msb = (lsb ^ (x >> 3) ^ (x << 4)) & 255
+    lsb = (x ^ (x << 5)) & 255
+  end
+
+  (msb << 8) + lsb
+end
+
+socket = SerialPort.new('/dev/ttyUSB0', 19200)
+socket.read_timeout = 1200
+
+3.times do |i|
+  socket.write("\n")
+  break if socket.read(2) == "\n\r"
+
+  if i == 2
+    puts 'Station not ready'
+    socket.close
+    exit 1
+  end
+end
+
+socket.write("LOOP 1\n")
+packet = socket.read(100)
+socket.close
+
+if packet.nil? || packet.size < 100
+  puts 'Received incomplete data'
+  exit 1
+end
+
+data = packet.match(/\A\x06(LOO.+\n\r.{2})\z/m)
+if data.nil? || crc16_ccitt(data[1]) != 0
+  puts 'Received malformed packet'
+  return
+end
+
+# Extract bytes 8-9, 13-14, 15, 34, 90 (little-endian)
+decoded = data[1].unpack('@7v@12vC@33C@89C')
+
+Net::HTTP.post(
+  URI(ARGV[0]),
+  JSON.dump(
+    state: Forecasts[decoded[4]],
+    attributes: {
+      friendly_name: 'Campus Weather',
+      pressure: decoded[0] / 1000.0,
+      temperature: decoded[1] / 10.0,
+      wind_speed: decoded[2],
+      humidity: decoded[3],
+      last_update: Time.now.strftime('%FT%T%z'),
+    }
+  ),
+  'Authorization' => "Bearer #{ENV['TOKEN']}",
+  'Content-Type' => 'application/json',
+)
